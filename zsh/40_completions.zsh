bcb6a6547403d6e49b33a8bcaae872c2671decd8
sumiya <sumiya@argama.localhost>
2016-10-23 22:03:39 +0900

    zsh-completion mac if else

--- zsh/40_completions.zsh
+++ zsh/40_completions.zsh
@@ -3,16 +3,20 @@
 # zsh-completionsを利用する Github => zsh-completions
 
 case ${OSTYPE} in
-    darwin*)
-        fpath=(/usr/local/share/zsh-completions $fpath)
-        autoload -U compinit
-        compinit -u
-        ;;
-    linux*)
-        fpath=(~/.zsh/zsh-completions/src $fpath)
-        autoload -U compinit;compinit
-        zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
-                             /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin \
-                             /usr/local/git/bin
-        ;;
+  darwin*)
+    if [ -e /usr/local/share/zsh-completions ]; then
+      fpath=(/usr/local/share/zsh-completions $fpath)
+      autoload -U compinit
+      compinit -u
+    fi
+    ;;
+  linux*)
+    if [ -e ~/.zsh/zsh-completions/src ]; then
+      fpath=(~/.zsh/zsh-completions/src $fpath)
+      autoload -U compinit;compinit
+      zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
+                           /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin \
+                           /usr/local/git/bin
+    fi
+    ;;
 esac
